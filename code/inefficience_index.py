0a78061ae2e2404fdbae33a44b288e8619a71713
aremiat <al.rem@outlook.fr>
2025-05-20 15:52:25 +0200

    follow up

--- code/inefficience_index.py
+++ code/inefficience_index.py
@@ -72,20 +72,20 @@ def hurst_swv(window: np.ndarray | pd.Series,
 
 if __name__ == "__main__":
     # Chargement des données pour '^RUT'
-    window_mfdfa = 1008
+    window_mfdfa = 252
     q_list = np.linspace(-4, 4, 17)
     scales = np.unique(np.logspace(np.log10(10), np.log10(50), 10, dtype=int))
     tickers = ["^FCHI", "^GSPC", "^RUT", "^FTSE", "^N225"]
     multi_asset_tickers = ['BTC-USD', 'EURUSD=X', 'GBPUSD=X']
-    # data =  pd.read_csv(os.path.join(DATA_PATH, "multi_assets.csv"), index_col=0, parse_dates=True)
+    data =  pd.read_csv(os.path.join(DATA_PATH, "multi_assets.csv"), index_col=0, parse_dates=True)
     # data = pd.read_csv(os.path.join(DATA_PATH, "index_prices2.csv"), index_col=0, parse_dates=True)
-    data = pd.read_csv(os.path.join(DATA_PATH, "ssec.csv"), index_col=0, parse_dates=True)
+    # data = pd.read_csv(os.path.join(DATA_PATH, "ssec.csv"), index_col=0, parse_dates=True)
 
     # tickers = ["^BVSP","^MXX", "000001.SS"]
     # data = yf.download(tickers, start="2000-01-01")  # prix journaliers
     # data = data.xs("Close", level=data.columns.names.index('Price'), axis=1)
 
-    for tick in data.columns:
+    for tick in multi_asset_tickers:
         # print(tick)
         # if tick == '^RUT':
         #     name = "Russel 2000"
@@ -118,7 +118,12 @@ if __name__ == "__main__":
             #     raw=False
             # ).dropna()
 
-            rolling_hurst = rolling_hurst_dfa(returns, window = 120)
+
+            rolling_hurst = returns.rolling(120).apply(lambda x : hurst_swv(x,  method="SD", exclusions=True),
+                                                       raw=True).dropna()
+
+
+            # rolling_hurst = rolling_hurst_dfa(returns, window = 120)
 
             np.random.seed(42)
             surrogate_returns = ComputeMFDFA.surrogate_gaussian_corr(returns.values)
@@ -144,21 +149,37 @@ if __name__ == "__main__":
             spectrum_width = spectrum_width.loc[spectrum_width.index.intersection(rolling_hurst.index)]
             prices_aligned = log_prices.loc[log_prices.index.intersection(spectrum_width.index)]
 
+            fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                                vertical_spacing=0.1,
+                                row_heights=[0.7, 0.3],
+                                subplot_titles=(f"{name} Price Evolution", "rolling_hurst"))
+            fig.add_trace(go.Scatter(x=prices_aligned.index, y=prices_aligned, mode='lines', name=f'{name} Price',
+                                     line=dict(color='red')),
+                          row=1, col=1)
+            fig.add_trace(go.Scatter(x=rolling_hurst.index, y=rolling_hurst, mode='lines', name='rolling_hurst',
+                                     line=dict(color='green')), row=2, col=1)
 
-            # fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
-            #                     vertical_spacing=0.1,
-            #                     row_heights=[0.7, 0.3],
-            #                     subplot_titles=(f"{name} Price Evolution", "Spectrum Width"))
-            # fig.add_trace(go.Scatter(x=prices_aligned.index, y=prices_aligned, mode='lines', name=f'{name} Price', line=dict(color='red')),
-            #               row=1, col=1)
-            # fig.add_trace(go.Scatter(x=spectrum_width.index, y=spectrum_width, mode='lines', name='Spectrum Width',
-            #                          line=dict(color='green')), row=2, col=1)
-            #
-            # fig.update_layout(title_text=f"{name} Analysis", showlegend=True)
-            # fig.update_xaxes(title_text="Date")
-            # fig.update_yaxes(title_text="Log Price ($)", row=1, col=1)
-            # fig.update_yaxes(title_text="Spectrum Width", row=2, col=1)
-            # fig.show()
+            fig.update_layout(title_text=f"{name} Analysis", showlegend=True)
+            fig.update_xaxes(title_text="Date")
+            fig.update_yaxes(title_text="Log Price ($)", row=1, col=1)
+            fig.update_yaxes(title_text="rolling_hurst", row=2, col=1)
+            fig.show()
+
+
+            fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                                vertical_spacing=0.1,
+                                row_heights=[0.7, 0.3],
+                                subplot_titles=(f"{name} Price Evolution", "Spectrum Width"))
+            fig.add_trace(go.Scatter(x=prices_aligned.index, y=prices_aligned, mode='lines', name=f'{name} Price', line=dict(color='red')),
+                          row=1, col=1)
+            fig.add_trace(go.Scatter(x=spectrum_width.index, y=spectrum_width, mode='lines', name='Spectrum Width',
+                                     line=dict(color='green')), row=2, col=1)
+
+            fig.update_layout(title_text=f"{name} Analysis", showlegend=True)
+            fig.update_xaxes(title_text="Date")
+            fig.update_yaxes(title_text="Log Price ($)", row=1, col=1)
+            fig.update_yaxes(title_text="Spectrum Width", row=2, col=1)
+            fig.show()
 
             common_date = rolling_hurst.index.intersection(spectrum_width.index)
             spectrum_width_aligned = spectrum_width.loc[common_date]
